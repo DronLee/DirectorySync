653f64345924169e6e9de91cb6086d83c7f8d509
avolkov <avolkov@movista.ru>
2019-06-05 18:12:53 +0300

    Добавил почти пустой проект. С парой намеченных интерфейсов.

---
+++ DirectorySync/Models/IDirectory.cs
@@ -0,0 +1,13 @@
+﻿namespace DirectorySync.Models
+{
+    /// <summary>
+    /// Директория.
+    /// </summary>
+    internal interface IDirectory : IItem
+    {
+        /// <summary>
+        /// Коллекция элементов в директории.
+        /// </summary>
+        IItem[] Items { get; }
+    }
+}
---
+++ DirectorySync/Models/IItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DirectorySync.Models
+{
+    /// <summary>
+    /// Элемент синхронизации.
+    /// </summary>
+    internal interface IItem
+    {
+        /// <summary>
+        /// Наименование элемента.
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Полный путь к элементу на диске.
+        /// </summary>
+        string FullPath { get; }
+
+        /// <summary>
+        /// Дата последнего обновления элемента.
+        /// </summary>
+        DateTime LastUpdate { get; }
+    }
+}
