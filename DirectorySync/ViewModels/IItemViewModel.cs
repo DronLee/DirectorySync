10174222783164efb77fde0d8af1a3b80c8e9596
dronlee <dronlee@list.ru>
2019-09-04 13:20:46 +0300

    Привёл в порядок модель представления отслежитваемого элемента. Убрал лишнее, добавил описание.

--- DirectorySync/ViewModels/IItemViewModel.cs
+++ DirectorySync/ViewModels/IItemViewModel.cs
@@ -4,14 +4,29 @@ using System.Windows.Input;
 
 namespace DirectorySync.ViewModels
 {
+    /// <summary>
+    /// Интерфейс модели представления отслеживаемого элемента.
+    /// </summary>
     public interface IItemViewModel : INotifyPropertyChanged
-    { 
+    {
+        /// <summary>
+        /// Наименование.
+        /// </summary>
         string Name { get; }
 
+        /// <summary>
+        /// Отображаемая моделью директория. Если модель отображает файл, то null.
+        /// </summary>
+        IDirectory Directory { get; }
+
+        /// <summary>
+        /// Статус элемента.
+        /// </summary>
         ItemStatus Status { get; set; }
 
+        /// <summary>
+        /// Выполняемая команда синхронизации. 
+        /// </summary>
         ICommand AcceptCommand { get; }
-
-        LoadedDirectory LoadedDirectoryEvent { get; set; }
     }
 }
--- DirectorySync/ViewModels/ItemViewModel.cs
+++ DirectorySync/ViewModels/ItemViewModel.cs
@@ -5,30 +5,77 @@ using DirectorySync.Models;
 
 namespace DirectorySync.ViewModels
 {
+    /// <summary>
+    /// Модель представления отслеживаемого элемента.
+    /// </summary>
     public class ItemViewModel : IItemViewModel
     {
         private readonly IItem _item;
 
-        public ItemViewModel(string name)
-        {
-            _item = null;
-            Name = name;
-        }
-
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="item">Отслеживаемый элемент, на основе которого создаётся модель.</param>
         public ItemViewModel(IItem item)
         {
             _item = item;
             Name = item.Name;
+            if (item is IDirectory)
+            {
+                Directory = (IDirectory)item;
+                Directory.LoadedDirectoryEvent += LoadedDirectory;
+            }
         }
 
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="item">Отслеживаемый элемент, на основе которого создаётся модель.</param>
+        /// <param name="itemStatusEnum">Статус, с которым будет создана модель.</param>
+        public ItemViewModel(IItem item, ItemStatusEnum itemStatusEnum) : this(item)
+        {
+            Status = new ItemStatus(itemStatusEnum);
+        }
+
+        /// <summary>
+        /// Конструктор создания отсутствующего элемента.
+        /// </summary>
+        /// <param name="name">Наименование отображаемого элемента.</param>
+        public ItemViewModel(string name)
+        {
+            _item = null;
+            Name = name;
+            Status = new ItemStatus(ItemStatusEnum.Missing);
+        }
+
+        /// <summary>
+        /// Наименование.
+        /// </summary>
         public string Name { get; }
 
-        public string IconPath { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
+        /// <summary>
+        /// Выполняемая команда синхронизации. 
+        /// </summary>
         public ICommand AcceptCommand => throw new NotImplementedException();
 
-        public ItemStatus Status { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        /// <summary>
+        /// Статус элемента.
+        /// </summary>
+        public ItemStatus Status { get; set; }
 
+        /// <summary>
+        /// Отображаемая моделью директория. Если модель отображает файл, то null.
+        /// </summary>
+        public IDirectory Directory { get; }
+
+        /// <summary>
+        /// Событие изменения одного из свойств модели.
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
+
+        private void LoadedDirectory(IDirectory directory)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Status)));
+        }
     }
 }
