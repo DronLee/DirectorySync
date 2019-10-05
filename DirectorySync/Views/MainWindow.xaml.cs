4ce9f8518f641cf15bbb58c6e56060fdf7893544
dronlee <dronlee@list.ru>
2019-10-05 21:42:00 +0300

    Реализовал синхронное передвижение скролла в левой и правой части.

--- DirectorySync/Views/MainWindow.xaml.cs
+++ DirectorySync/Views/MainWindow.xaml.cs
@@ -13,5 +13,17 @@ namespace DirectorySync.Views
             InitializeComponent();
             DataContext = viewModel;
         }
+
+        private void LeftTreeViewScroll_ScrollChanged(object sender, System.Windows.Controls.ScrollChangedEventArgs e)
+        {
+            RightTreeViewScroll.ScrollToVerticalOffset(e.VerticalOffset);
+            RightTreeViewScroll.ScrollToHorizontalOffset(e.HorizontalOffset);
+        }
+
+        private void RightTreeViewScroll_ScrollChanged(object sender, System.Windows.Controls.ScrollChangedEventArgs e)
+        {
+            LeftTreeViewScroll.ScrollToVerticalOffset(e.VerticalOffset);
+            LeftTreeViewScroll.ScrollToHorizontalOffset(e.HorizontalOffset);
+        }
     }
 }
