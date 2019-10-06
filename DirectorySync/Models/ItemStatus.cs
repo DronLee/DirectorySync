701d5482c3f5e5bf931190c5ec385a7110691509
dronlee <dronlee@list.ru>
2019-10-06 13:36:23 +0300

    Добавил отображение иконок для статусов элментов.

--- DirectorySync/Models/ItemStatus.cs
+++ DirectorySync/Models/ItemStatus.cs
@@ -1,9 +1,22 @@
-﻿namespace DirectorySync.Models
+﻿using System.Collections.Generic;
+
+namespace DirectorySync.Models
 {
     public class ItemStatus
     {
+        private readonly static Dictionary<ItemStatusEnum, string> _iconPathesOnStatus = new Dictionary<ItemStatusEnum, string>
+        {
+            { ItemStatusEnum.Equally, "/DirectorySync;component/Icons/Status/Equally.png" },
+            { ItemStatusEnum.Missing, "/DirectorySync;component/Icons/Status/Missing.png" },
+            { ItemStatusEnum.Newer, "/DirectorySync;component/Icons/Status/Newer.png" },
+            { ItemStatusEnum.Older, "/DirectorySync;component/Icons/Status/Older.png" },
+            { ItemStatusEnum.ThereIs, "/DirectorySync;component/Icons/Status/ThereIs.png" },
+            { ItemStatusEnum.Unknown, "/DirectorySync;component/Icons/Status/Unknown.png" }
+        };
+
         public readonly ItemStatusEnum StatusEnum;
-        public readonly string IconPath;
+
+        public string IconPath => _iconPathesOnStatus[StatusEnum];
 
         public ItemStatus(ItemStatusEnum statusEnum)
         {
