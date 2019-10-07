f39a429cc9a0ca3a359c8548ea0b6bcab0ac5e7b
dronlee <dronlee@list.ru>
2019-10-07 09:52:53 +0300

    Добавил тесты для модели файла.

---
+++ XUnitTestProject/FileTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Xunit;
+
+namespace XUnitTestProject
+{
+    /// <summary>
+    /// Тесты для модели файла.
+    /// </summary>
+    public class FileTests
+    {
+        [Fact]
+        public void CopyTo()
+        {
+            using (var testDirectory = new Infrastructure.TestDirectory())
+            {
+                var sourceFile = Path.Combine(testDirectory.FullPath, Guid.NewGuid().ToString());
+                var destinationFile = Path.Combine(testDirectory.FullPath, Guid.NewGuid().ToString());
+                var fileText = Guid.NewGuid().ToString();
+
+                File.WriteAllText(sourceFile, fileText);
+                var file = new DirectorySync.Models.File(sourceFile);
+                file.CopyTo(destinationFile).Wait();
+
+                Assert.True(File.Exists(sourceFile));
+                Assert.True(File.Exists(destinationFile));
+                Assert.Equal(fileText, File.ReadAllText(destinationFile));
+            }
+        }
+
+        [Fact]
+        public void Delete()
+        {
+            using (var testDirectory = new Infrastructure.TestDirectory())
+            {
+                var sourceFile = Path.Combine(testDirectory.FullPath, Guid.NewGuid().ToString());
+                File.WriteAllBytes(sourceFile, new byte[0]);
+                var file = new DirectorySync.Models.File(sourceFile);
+                file.Delete().Wait();
+
+                Assert.False(File.Exists(sourceFile));
+            }
+        }
+    }
+}
