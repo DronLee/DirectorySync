dc9fa77e085d14dec24b362702d4307f5089d3d8
dronlee <dronlee@list.ru>
2019-10-08 19:08:58 +0300

    Добавил тест на сохранение файла настроек и в тест на инициализацтю хранилища настроек добваил проверку определния NotFound для директорий.

--- XUnitTestProject/SettingsStorageTests.cs
+++ XUnitTestProject/SettingsStorageTests.cs
@@ -1,5 +1,7 @@
 ﻿using DirectorySync.Models.Settings;
+using Newtonsoft.Json;
 using System.IO;
+using System.Text;
 using Xunit;
 using XUnitTestProject.Infrastructure;
 
@@ -21,6 +23,13 @@ namespace XUnitTestProject
                 var storage = new SettingsStorage(settingsFilePath);
 
                 Assert.Equal(rowsCount, storage.SettingsRows.Length);
+
+                // Каждый раз при инициализации хранилище должно проверять какие директории есть, а каких нет.
+                foreach(var row in storage.SettingsRows)
+                {
+                    Assert.Equal(!Directory.Exists(row.LeftDirectory.DirectoryPath), row.LeftDirectory.NotFound);
+                    Assert.Equal(!Directory.Exists(row.RightDirectory.DirectoryPath), row.RightDirectory.NotFound);
+                }
             }
         }
 
@@ -39,6 +48,51 @@ namespace XUnitTestProject
             //При создании не проверяется наличие директории, создаётся, как будто существующая.
             Assert.False(result.LeftDirectory.NotFound);
             Assert.False(result.RightDirectory.NotFound);
+        }
+
+        [Fact]
+        public void Save()
+        {
+            using (var testDirectory = new TestDirectory())
+            {
+                var settingsFile = Path.Combine(testDirectory.FullPath, "TestSettings");
+                var storage = new SettingsStorage(settingsFile);
+                storage.SettingsRows = new[]
+                {
+                    new SettingsRow
+                    {
+                        LeftDirectory = new SettingsDirectory("1") { NotFound = true },
+                        RightDirectory = new SettingsDirectory("2") { NotFound = true },
+                        IsUsed = false
+                    },
+                    new SettingsRow
+                    {
+                        LeftDirectory = new SettingsDirectory("3") { NotFound = true },
+                        RightDirectory = new SettingsDirectory("4") { NotFound = true },
+                        IsUsed = true
+                    }
+                };
+                storage.Save();
+
+                Assert.True(File.Exists(settingsFile));
+
+                var settingsFileData = JsonConvert.DeserializeObject<SettingsRow[]>(
+                    File.ReadAllText(settingsFile, Encoding.UTF8));
+                Assert.Equal(storage.SettingsRows.Length, settingsFileData.Length);
+                for(int i=0; i< storage.SettingsRows.Length; i++)
+                {
+                    var expectedRow = storage.SettingsRows[i];
+                    var resultRow = settingsFileData[i];
+
+                    Assert.Equal(expectedRow.LeftDirectory.DirectoryPath, resultRow.LeftDirectory.DirectoryPath);
+                    Assert.Equal(expectedRow.RightDirectory.DirectoryPath, resultRow.RightDirectory.DirectoryPath);
+                    Assert.Equal(expectedRow.IsUsed, resultRow.IsUsed);
+
+                    // NotFound не хранится в файле, так как каждый раз при загрузке снова проверяется.
+                    Assert.False(resultRow.LeftDirectory.NotFound);
+                    Assert.False(resultRow.RightDirectory.NotFound);
+                }
+            }
         }
     }
 }
