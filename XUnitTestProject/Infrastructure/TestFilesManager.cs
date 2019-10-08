b3479e1fd6e28980de3d865eb6c44e3df2ab76a7
dronlee <dronlee@list.ru>
2019-10-08 18:09:21 +0300

    Добавлен тест на инициализацию хранилища настроек и в тестовом проекте теперь есть мэнеджер, отвечающий на тестовые файлы.

---
+++ XUnitTestProject/Infrastructure/TestFilesManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+
+namespace XUnitTestProject.Infrastructure
+{
+    public static class TestFilesManager
+    {
+        public static string TestFilesDirectory => Path.Combine(Environment.CurrentDirectory, "TestFiles");
+
+        public static string SettingsDirectory => Path.Combine(TestFilesDirectory, "Settings");
+    }
+}
---
+++ XUnitTestProject/SettingsStorageTests.cs
@@ -0,0 +1,27 @@
+﻿using DirectorySync.Models.Settings;
+using System.IO;
+using Xunit;
+using XUnitTestProject.Infrastructure;
+
+namespace XUnitTestProject
+{
+    public class SettingsStorageTests
+    {
+        [Theory]
+        [InlineData("NotExists", 0)]
+        [InlineData("2RowsSettings", 2)]
+        public void Init(string settingsFileName, byte rowsCount)
+        {
+            using (var testDirectory = new TestDirectory())
+            {
+                var settingsFilePath = Path.Combine(testDirectory.FullPath, settingsFileName);
+                foreach (string file in Directory.GetFiles(TestFilesManager.SettingsDirectory, settingsFileName))
+                    File.Copy(file, settingsFilePath);
+
+                var storage = new SettingsStorage(settingsFilePath);
+
+                Assert.Equal(rowsCount, storage.SettingsRows.Length);
+            }
+        }
+    }
+}
