0f3724e9f47b026b9aa6c35606a1d0a13fa1c615
dronlee <dronlee@list.ru>
2019-10-14 11:26:24 +0300

    Фабрика элементом теперь может возвращать null и добавлено описание.

--- DirectorySync/Models/IItemFactory.cs
+++ DirectorySync/Models/IItemFactory.cs
@@ -1,9 +1,22 @@
 ﻿namespace DirectorySync.Models
 {
+    /// <summary>
+    /// Интерфейс фабрики отслеживаемых элементов.
+    /// </summary>
     public interface IItemFactory
     {
+        /// <summary>
+        /// Создание директории.
+        /// </summary>
+        /// <param name="directoryPath">Полный путь к директории.</param>
+        /// <returns>Созданная директория.</returns>
         IDirectory CreateDirectory(string directoryPath);
 
+        /// <summary>
+        /// Создание файла.
+        /// </summary>
+        /// <param name="filePath">Полный путь к файлу.</param>
+        /// <returns>Созданный файл.</returns>
         IItem CreateFile(string filePath);
     }
 }
--- DirectorySync/Models/ItemFactory.cs
+++ DirectorySync/Models/ItemFactory.cs
@@ -1,15 +1,42 @@
 ﻿namespace DirectorySync.Models
 {
+    /// <summary>
+    /// Фабрика отслеживаемых элементов.
+    /// </summary>
     public class ItemFactory : IItemFactory
     {
+        /// <summary>
+        /// Создание директории.
+        /// </summary>
+        /// <param name="directoryPath">Полный путь к директории.</param>
+        /// <returns>Созданная директория. Если null, значит директорию не удалось найти.</returns>
         public IDirectory CreateDirectory(string directoryPath)
         {
-            return new Directory(directoryPath, this);
+            try
+            {
+                return new Directory(directoryPath, this);
+            }
+            catch
+            {
+                return null;
+            }
         }
 
+        /// <summary>
+        /// Создание файла.
+        /// </summary>
+        /// <param name="filePath">Полный путь к файлу.</param>
+        /// <returns>Созданный файл. Если null значит файл не удалось найти.</returns>
         public IItem CreateFile(string filePath)
         {
-            return new File(filePath);
+            try
+            {
+                return new File(filePath);
+            }
+            catch
+            {
+                return null;
+            }
         }
     }
 }
