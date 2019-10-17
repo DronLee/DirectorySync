521d58475240ced7a9feb405ba0607b51a8cf569
dronlee <dronlee@list.ru>
2019-10-17 22:22:52 +0300

    Доработаны тесты для модели представления строки. 1. Убраны тесты на работу события удаления строки, так как этого события больше нет. 2. Учитывается, что событие на копирование элемента больше не передаёт исходный элемент. 3. Учитывается, что команды синхронизации должны возврашщать Task. 4. Учитываются новые правила видимости кнопок команд и видимость заставки "в процессе".

--- XUnitTestProject/RowViewModelTests.cs
+++ XUnitTestProject/RowViewModelTests.cs
@@ -87,13 +87,13 @@ namespace XUnitTestProject
 
                 // Запишем команды синхронизации для каждого элемента и подпишемся на их выполнение.
                 leftItemViewModel.SetActionCommand(() => leftItemViewModel.Item.CopyTo(rightItemViewModel.FullPath));
-                leftItemViewModel.Item.CopiedFromToEvent += (IItem item1, IItem item2, string destinationPath) =>
+                leftItemViewModel.Item.CopiedFromToEvent += (IItem item1, string destinationPath) =>
                 {
                     lock (resultCopyDestinationPathesLocker)
                         resultCopyDestinationPathes.Add(destinationPath);
                 };
                 rightItemViewModel.SetActionCommand(() => rightItemViewModel.Item.CopyTo(leftItemViewModel.FullPath));
-                rightItemViewModel.Item.CopiedFromToEvent += (IItem item1, IItem item2, string destinationPath) =>
+                rightItemViewModel.Item.CopiedFromToEvent += (IItem item1, string destinationPath) =>
                 {
                     lock (resultCopyDestinationPathesLocker)
                         resultCopyDestinationPathes.Add(destinationPath);
@@ -127,120 +127,6 @@ namespace XUnitTestProject
         }
 
         /// <summary>
-        /// Проверка возникновения события на удаление строки, при прнятии левого элемента, который отсутсвует.
-        /// </summary>
-        [Fact]
-        public void DeleteRowEventAfterLeftAccept()
-        {
-            using (var testDirectory = new Infrastructure.TestDirectory())
-            {
-                var leftParentDirectoryPath = testDirectory.CreateDirectory("1");
-                var rightParentDirectoryPath = testDirectory.CreateDirectory("1");
-
-                var rightDirectoryPath = System.IO.Path.Combine(rightParentDirectoryPath, "2");
-                System.IO.Directory.CreateDirectory(rightDirectoryPath);
-
-                var rightDirectory = new Directory(rightDirectoryPath, null);
-
-                var filesDictionary = new Dictionary<string, DateTime>();
-                for (byte i = 0; i < filesDictionary.Count; i++)
-                    filesDictionary.Add(i.ToString(), DateTime.Now);
-
-                Infrastructure.TestDirectory.CreateFiles(rightDirectory.FullPath, filesDictionary);
-
-                var parentRow = new RowViewModel(
-                    new ItemViewModel(leftParentDirectoryPath, true, new Directory(leftParentDirectoryPath, null)),
-                    new ItemViewModel(rightParentDirectoryPath, true, new Directory(rightParentDirectoryPath, null)), null);
-
-                var leftItemViewModel = new ItemViewModel("Test", true, null);
-                leftItemViewModel.SetActionCommand(async () => { await rightDirectory.Delete(); });
-                var rowViewModel = new RowViewModel(leftItemViewModel,
-                    new ItemViewModel(rightDirectory.FullPath, true, rightDirectory), parentRow);
-
-                foreach (var fileName in filesDictionary.Keys)
-                {
-                    var rightItem = new File(System.IO.Path.Combine(rightDirectory.FullPath, fileName));
-                    rowViewModel.ChildRows.Add(new RowViewModel(
-                        new ItemViewModel(fileName, false, null),
-                        new ItemViewModel(rightItem.FullPath, false, rightItem),
-                        rowViewModel));
-                }
-
-                IRowViewModel deletedRow = null; // Удалённая строка.
-                IRowViewModel parentDeletedRow = null; // Родительская строка удалённой строки.
-                rowViewModel.DeleteRowViewModelEvent += (IRowViewModel deletingRow, IRowViewModel parentDeletingRow) =>
-                {
-                    deletedRow = deletingRow;
-                    parentDeletedRow = parentDeletingRow;
-                };
-
-                rowViewModel.LeftItem.AcceptCommand.Execute(null);
-
-                Thread.Sleep(50); // Чтобы успело выполниться событие.
-                Assert.NotNull(deletedRow);
-                Assert.Equal(rowViewModel, deletedRow);
-                Assert.Equal(parentRow, parentDeletedRow);
-            }
-        }
-
-        /// <summary>
-        /// Проверка возникновения события на удаление строки, при прнятии правого элемента, который отсутсвует.
-        /// </summary>
-        [Fact]
-        public void DeleteRowEventAfterRightAccept()
-        {
-            using (var testDirectory = new Infrastructure.TestDirectory())
-            {
-                var leftParentDirectoryPath = testDirectory.CreateDirectory("1");
-                var rightParentDirectoryPath = testDirectory.CreateDirectory("1");
-
-                var leftDirectoryPath = System.IO.Path.Combine(leftParentDirectoryPath, "2");
-                System.IO.Directory.CreateDirectory(leftDirectoryPath);
-
-                var leftDirectory = new Directory(leftDirectoryPath, null);
-
-                var filesDictionary = new Dictionary<string, DateTime>();
-                for (byte i = 0; i < filesDictionary.Count; i++)
-                    filesDictionary.Add(i.ToString(), DateTime.Now);
-
-                Infrastructure.TestDirectory.CreateFiles(leftDirectory.FullPath, filesDictionary);
-
-                var parentRow = new RowViewModel(
-                    new ItemViewModel(leftParentDirectoryPath, true, new Directory(leftParentDirectoryPath, null)),
-                    new ItemViewModel(rightParentDirectoryPath, true, new Directory(rightParentDirectoryPath, null)), null);
-
-                var rightItemViewModel = new ItemViewModel("Test", true, null);
-                rightItemViewModel.SetActionCommand(async () => { await leftDirectory.Delete(); });
-                var rowViewModel = new RowViewModel(
-                    new ItemViewModel(leftDirectory.FullPath, true, leftDirectory),
-                    rightItemViewModel, parentRow);
-
-                foreach (var fileName in filesDictionary.Keys)
-                {
-                    var leftItem = new File(System.IO.Path.Combine(leftDirectory.FullPath, fileName));
-                    rowViewModel.ChildRows.Add(new RowViewModel(
-                        new ItemViewModel(leftItem.FullPath, false, leftItem),
-                        new ItemViewModel(fileName, false, null), parentRow));
-                }
-
-                IRowViewModel deletedRow = null; // Удалённая строка.
-                IRowViewModel parentDeletedRow = null; // Родительская строка удалённой строки.
-                rowViewModel.DeleteRowViewModelEvent += (IRowViewModel deletingRow, IRowViewModel parentDeletingRow) =>
-                {
-                    deletedRow = deletingRow;
-                    parentDeletedRow = parentDeletingRow;
-                };
-
-                rowViewModel.RightItem.AcceptCommand.Execute(null);
-
-                Thread.Sleep(50); // Чтобы успело выполниться событие.
-                Assert.NotNull(deletedRow);
-                Assert.Equal(rowViewModel, deletedRow);
-                Assert.Equal(parentRow, parentDeletedRow);
-            }
-        }
-
-        /// <summary>
         /// Тест на выполнение команды принятия левого элемента.
         /// </summary>
         [Fact]
@@ -249,16 +135,12 @@ namespace XUnitTestProject
             var useAcceptCommand = false;
             var leftItemViewModel = new ItemViewModel("LeftItem", false, null);
             var rightItemViewModel = new ItemViewModel("RightItem", false, null);
-            leftItemViewModel.SetActionCommand(() => { useAcceptCommand = true; });
+            leftItemViewModel.SetActionCommand(() => Task.Run(() => { useAcceptCommand = true; }) );
             var rowViewModel = new RowViewModel(leftItemViewModel, rightItemViewModel, null);
             rowViewModel.LeftItem.AcceptCommand.Execute(null);
             Thread.Sleep(15); //  Чтобы успела выполниться команда.
 
             Assert.True(useAcceptCommand);
-
-            // Сразу после запуска выполнения команды должна удалиться.
-            Assert.Null(rowViewModel.LeftItem.AcceptCommand);
-            Assert.Null(rowViewModel.RightItem.AcceptCommand);
         }
 
         /// <summary>
@@ -270,16 +152,12 @@ namespace XUnitTestProject
             var useAcceptCommand = false;
             var leftItemViewModel = new ItemViewModel("LeftItem", false, null);
             var rightItemViewModel = new ItemViewModel("RightItem", false, null);
-            rightItemViewModel.SetActionCommand(() => { useAcceptCommand = true; });
+            rightItemViewModel.SetActionCommand(() => Task.Run(() => { useAcceptCommand = true; }));
             var rowViewModel = new RowViewModel(leftItemViewModel, rightItemViewModel, null);
             rowViewModel.RightItem.AcceptCommand.Execute(null);
             Thread.Sleep(15); //  Чтобы успела выполниться команда.
 
             Assert.True(useAcceptCommand);
-
-            // Сразу после запуска выполнения команды должна удалиться.
-            Assert.Null(rowViewModel.LeftItem.AcceptCommand);
-            Assert.Null(rowViewModel.RightItem.AcceptCommand);
         }
 
         /// <summary>
@@ -289,11 +167,14 @@ namespace XUnitTestProject
         public void VisibilityAcceptButton_Init()
         {
             var leftItemViewModel = new ItemViewModel("LeftItem", false, null);
-            leftItemViewModel.SetActionCommand(() => { });
+            leftItemViewModel.SetActionCommand(() => Task.Run(() => { }));
             var rightItemViewModel = new ItemViewModel("RightItem", false, null);
             var rowViewModel = new RowViewModel(leftItemViewModel, rightItemViewModel, null);
 
-            Assert.True(rowViewModel.CommandButtonIsVisible);
+            // Когда строка только создана, она считается ещё в процессе загрузки. Этот признак должен быть потом убран.
+            Assert.True(rowViewModel.InProcess);
+            // Соответсвенно, пока строка "в процессе", кнопки синхронизации не должны быть доступны.
+            Assert.False(rowViewModel.CommandButtonIsVisible);
         }
 
         /// <summary>
@@ -305,13 +186,13 @@ namespace XUnitTestProject
             var leftItemViewModel = new ItemViewModel("LeftItem", false, null);
             var rightItemViewModel = new TestItemViewModel("RightItem", ItemStatusEnum.Older);
             // Sleep, чтобы была возможность проверить свойства в процессе выполнения синхронизации.
-            leftItemViewModel.SetActionCommand(() => { Thread.Sleep(70); });
+            leftItemViewModel.SetActionCommand(() => { Thread.Sleep(70); return Task.FromResult(true); });
             var rowViewModel = new RowViewModel(leftItemViewModel, rightItemViewModel, null);
             rowViewModel.LeftItem.AcceptCommand.Execute(null);
             Thread.Sleep(25); //  Чтобы успели обновиться свойства.
 
             Assert.False(rowViewModel.CommandButtonIsVisible);
-            Assert.True(rowViewModel.ProcessIconIsVisible);
+            Assert.True(rowViewModel.InProcess);
         }
 
         /// <summary>
@@ -323,13 +204,13 @@ namespace XUnitTestProject
             var leftItemViewModel = new TestItemViewModel("LeftItem", ItemStatusEnum.Older);
             var rightItemViewModel = new ItemViewModel("RightItem", false, null);
             // Sleep, чтобы была возможность проверить свойства в процессе выполнения синхронизации.
-            rightItemViewModel.SetActionCommand(() => { Thread.Sleep(70); });
+            rightItemViewModel.SetActionCommand(() => { Thread.Sleep(70); return Task.FromResult(true); });
             var rowViewModel = new RowViewModel(leftItemViewModel, rightItemViewModel, null);
             rowViewModel.RightItem.AcceptCommand.Execute(null);
             Thread.Sleep(25); //  Чтобы успели обновиться свойства.
 
             Assert.False(rowViewModel.CommandButtonIsVisible);
-            Assert.True(rowViewModel.ProcessIconIsVisible);
+            Assert.True(rowViewModel.InProcess);
         }
 
         /// <summary>
@@ -338,15 +219,16 @@ namespace XUnitTestProject
         [Fact]
         public void VisibilityAcceptButton_FinishedLeftItemAccept()
         {
-            var leftItemViewModel = new ItemViewModel("LeftItem", false, null);
-            leftItemViewModel.SetActionCommand(() => { });
-            var rightItemViewModel = new TestItemViewModel("RightItem", ItemStatusEnum.Older);
+            var leftItemViewModel = new ItemViewModel("LeftItem", false, new TestItem());
+            leftItemViewModel.SetActionCommand(() => { return Task.FromResult(true); });
+            var rightItemViewModel = new ItemViewModel("RightItem", false, new TestItem());
+            rightItemViewModel.SetActionCommand(() => { return Task.FromResult(true); });
             var rowViewModel = new RowViewModel(leftItemViewModel, rightItemViewModel, null);
             rowViewModel.LeftItem.AcceptCommand.Execute(null);
             Thread.Sleep(25); //  Чтобы успели обновиться свойства.
 
-            Assert.False(rowViewModel.CommandButtonIsVisible);
-            Assert.False(rowViewModel.ProcessIconIsVisible);
+            Assert.True(rowViewModel.CommandButtonIsVisible);
+            Assert.False(rowViewModel.InProcess);
         }
 
         /// <summary>
@@ -355,15 +237,16 @@ namespace XUnitTestProject
         [Fact]
         public void VisibilityAcceptButton_FinishedRightItemAccept()
         {
-            var leftItemViewModel = new TestItemViewModel("LeftItem", ItemStatusEnum.Older);
-            var rightItemViewModel = new ItemViewModel("RightItem", false, null);
-            rightItemViewModel.SetActionCommand(() => { });
+            var leftItemViewModel = new ItemViewModel("LeftItem", false, new TestItem());
+            leftItemViewModel.SetActionCommand(() => { return Task.FromResult(true); });
+            var rightItemViewModel = new ItemViewModel("RightItem", false, new TestItem());
+            rightItemViewModel.SetActionCommand(() => { return Task.FromResult(true); });
             var rowViewModel = new RowViewModel(leftItemViewModel, rightItemViewModel, null);
             rowViewModel.RightItem.AcceptCommand.Execute(null);
             Thread.Sleep(25); // Чтобы успели обновиться свойства.
 
-            Assert.False(rowViewModel.CommandButtonIsVisible);
-            Assert.False(rowViewModel.ProcessIconIsVisible);
+            Assert.True(rowViewModel.CommandButtonIsVisible);
+            Assert.False(rowViewModel.InProcess);
         }
 
         private class TestItemViewModel : IItemViewModel
@@ -380,7 +263,7 @@ namespace XUnitTestProject
 
             public ItemStatus Status { get; private set; }
 
-            public Action CommandAction { get; }
+            public Func<Task> CommandAction { get; }
 
             public ICommand AcceptCommand { get; set; }
 
@@ -392,14 +275,14 @@ namespace XUnitTestProject
 
             public string FullPath => throw new NotImplementedException();
 
-            public event PropertyChangedEventHandler PropertyChanged;
-            public event Action StartedSyncEvent;
-            public event Action FinishedSyncEvent;
-            public event Action ItemIsDeletedEvent;
-            public event Action<IItemViewModel, IItemViewModel> CopiedFromToEvent;
             public event Action AcceptCommandChangedEvent;
+            public event Action StartedSyncEvent;
+            public event Action<IItemViewModel> FinishedSyncEvent;
+            public event Action<IItemViewModel, IItemViewModel> CopiedFromToEvent;
+            public event PropertyChangedEventHandler PropertyChanged;
+            public event Action<string> SyncErrorEvent;
 
-            public void SetActionCommand(Action action) { }
+            public void SetActionCommand(Func<Task> action) { }
 
             public void UpdateStatus(ItemStatusEnum statusEnum)
             {
@@ -417,11 +300,11 @@ namespace XUnitTestProject
 
             public event Action DeletedEvent;
             public event Action<string> SyncErrorEvent;
-            public event Action<IItem, IItem, string> CopiedFromToEvent;
+            public event Action<IItem, string> CopiedFromToEvent;
 
             public Task CopyTo(string destinationPath)
             {
-                return Task.Run(() => CopiedFromToEvent?.Invoke(null, null, destinationPath));
+                return Task.Run(() => CopiedFromToEvent?.Invoke(null, destinationPath));
             }
 
             public Task Delete()
