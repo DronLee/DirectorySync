676e88636221b2bf424b53bb3ad0ee749128bfc8
dronlee <dronlee@list.ru>
2019-11-28 16:46:46 +0300

    Добавил тесты на срабатывание события DeleteRowEvent у фабрики создания моделей представлений строк.

--- XUnitTestProject/RowViewModelFactoryTests.cs
+++ XUnitTestProject/RowViewModelFactoryTests.cs
@@ -90,6 +90,146 @@ namespace XUnitTestProject
             }
         }
 
+        /// <summary>
+        /// Проверка срабатывания события удаления строки при удалении синхронизируемых элементов директорий.
+        /// </summary>
+        [Fact]
+        public async Task DeleteDirectoryRowEvent()
+        {
+            IRowViewModel deleteRowEventParent = null, deleteRowEventChild = null;
+
+            using (var testDirectory = new Infrastructure.TestDirectory())
+            {
+                Infrastructure.TestDirectory.CreateFiles(testDirectory.CreateDirectory("Dir"), new Dictionary<string, DateTime>
+                {
+                    { "File", DateTime.Now }
+                });
+
+                var synchronizedDirectories = new TestSynchronizedItems(new Directory(testDirectory.FullPath, null, new ItemFactory()));
+                var factory = new RowViewModelFactory();
+                factory.DeleteRowEvent += (IRowViewModel parent, IRowViewModel child) =>
+                {
+                    deleteRowEventParent = parent;
+                    deleteRowEventChild = child;
+                };
+
+                var rowViewModel = factory.CreateRowViewModel(synchronizedDirectories);
+
+                await synchronizedDirectories.Load();
+
+                var level1Child = synchronizedDirectories.ChildItems[0];
+                var level2Child = level1Child.ChildItems[0];
+
+                ((TestSynchronizedItems)level1Child).Delete();
+
+                Assert.NotNull(deleteRowEventParent);
+                Assert.NotNull(deleteRowEventChild);
+                Assert.Equal(synchronizedDirectories.LeftItem.Name, deleteRowEventParent.LeftItem.Name);
+                Assert.Equal(level1Child.LeftItem.Name, deleteRowEventChild.LeftItem.Name);
+            }
+        }
+
+        /// <summary>
+        /// Проверка срабатывания события удаления строки при удалении синхронизируемых элементов файлов.
+        /// </summary>
+        [Fact]
+        public async Task DeleteFileRowEvent()
+        {
+            IRowViewModel deleteRowEventParent = null, deleteRowEventChild = null;
+
+            using (var testDirectory = new Infrastructure.TestDirectory())
+            {
+                Infrastructure.TestDirectory.CreateFiles(testDirectory.CreateDirectory("Dir"), new Dictionary<string, DateTime>
+                {
+                    { "File", DateTime.Now }
+                });
+
+                var synchronizedDirectories = new TestSynchronizedItems(new Directory(testDirectory.FullPath, null, new ItemFactory()));
+                var factory = new RowViewModelFactory();
+                factory.DeleteRowEvent += (IRowViewModel parent, IRowViewModel child) => 
+                {
+                    deleteRowEventParent = parent;
+                    deleteRowEventChild = child;
+                };
+
+                var rowViewModel = factory.CreateRowViewModel(synchronizedDirectories);
+
+                await synchronizedDirectories.Load();
+
+                var level1Child = synchronizedDirectories.ChildItems[0];
+                var level2Child = level1Child.ChildItems[0];
+
+                ((TestSynchronizedItems)level2Child).Delete();
+
+                Assert.NotNull(deleteRowEventParent);
+                Assert.NotNull(deleteRowEventChild);
+                Assert.Equal(level1Child.LeftItem.Name, deleteRowEventParent.LeftItem.Name);
+                Assert.Equal(level2Child.LeftItem.Name, deleteRowEventChild.LeftItem.Name);
+            }
+        }
+
+        /// <summary>
+        /// Упрощённая модель синхронизируемых элементов, в которой не важно содержимое, главное,
+        /// чтобы работала загрузка, создавая дочерние элементы, и была возможность активировать событие удаления.
+        /// </summary>
+        private class TestSynchronizedItems : ISynchronizedItems
+        {
+            private readonly IItem _item;
+
+            public TestSynchronizedItems(IItem item)
+            {
+                _item = item;
+                LeftItem = new SynchronizedItem(item.FullPath, item is IDirectory, item);
+                RightItem = new SynchronizedItem(item.FullPath, item is IDirectory, item);
+            }
+
+            public bool IsLoaded => throw new NotImplementedException();
+
+            public IDirectory LeftDirectory => throw new NotImplementedException();
+
+            public IDirectory RightDirectory => throw new NotImplementedException();
+
+            public ISynchronizedItem LeftItem { get; }
+
+            public ISynchronizedItem RightItem { get; }
+
+            public List<ISynchronizedItems> ChildItems { get; private set; } = new List<ISynchronizedItems>();
+
+            public event Action<ISynchronizedItems> DirectoriesIsLoadedEvent;
+            public event Action<ISynchronizedItems> DeleteEvent;
+
+            public async Task Load()
+            {
+                await _item.Load();
+                LoadChildItems();
+                DirectoriesIsLoadedEvent?.Invoke(this);
+            }
+
+            public void LoadChildItems()
+            {
+                if (_item is IDirectory)
+                    foreach (var item in ((IDirectory)_item).Items)
+                    {
+                        var child = new TestSynchronizedItems(item);
+                        ChildItems.Add(child);
+                        child.LoadChildItems();
+                    }
+            }
+
+            public void LoadRequired()
+            {
+                throw new NotImplementedException();
+            }
+
+            /// <summary>
+            /// Нужен, чтобы проверить реакцию на событие удаления синхронизируемых элементов.
+            /// </summary>
+            public void Delete()
+            {
+                DeleteEvent?.Invoke(this);
+            }
+        }
+
         private SynchronizedItems GetSynchronizedDirectories(string leftDirectoryPath, string rightDirectoryPath)
         {
             var settingsRow = new TestSettingsRow
