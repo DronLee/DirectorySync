96e34b16b883d028271d4dd69d16bf94cfe7eebc
dronlee <dronlee@list.ru>
2019-12-03 20:09:10 +0300

    Добавил в проект unit-тестов настройку Autofac и поддержку его через FakeItEasy.

---
+++ XUnitTestProject/TestAutofacRegisterModule.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using DirectorySync.Models;
+using DirectorySync.Models.Settings;
+
+namespace XUnitTestProject
+{
+    internal class TestAutofacRegisterModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<SettingsStorage>().As<ISettingsStorage>().SingleInstance()
+                .WithParameter("settingsFile", "Settings");
+
+            builder.RegisterType<ItemFactory>().As<IItemFactory>().SingleInstance();
+            builder.RegisterType<SynchronizedItemFactory>().As<ISynchronizedItemFactory>().SingleInstance();
+            builder.RegisterType<SynchronizedItemMatcher>().As<ISynchronizedItemMatcher>().SingleInstance();
+            builder.RegisterType<SynchronizedItemsStatusAndCommandsUpdater>()
+                .As<ISynchronizedItemsStatusAndCommandsUpdater>().SingleInstance();
+            builder.RegisterType<SynchronizedDirectoriesManager>().As<ISynchronizedDirectoriesManager>().SingleInstance();
+        }
+    }
+}
